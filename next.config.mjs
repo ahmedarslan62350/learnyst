2c5f960edf7d9750567a3feebd0c693c2abb9046
ahmedarslan62350 <ahmedarslana62350@gmail.com>
2025-08-18 20:24:34 +0500

    feat: V2.0.5

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "40mb", // adjust as needed
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb", // Adjust as needed
     },
   },
 };
